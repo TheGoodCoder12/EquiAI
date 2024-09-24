8820d4ecd3011bd9dd4ec01fd3bb5543574bf901
TheGoodCoder12 <satyam.strange93@gmail.com>
2024-09-24 19:45:11 +0530

    Front end changes and creation of .env file

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
-const path = require('path');
 const app = express();
-const port = 3000;
+const path = require('path');
+
+const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.get('/welcome', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'templates', 'welcome.html'));
 });
 app.listen(port, () => {
