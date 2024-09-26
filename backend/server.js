6579931ae8d670702bf329c145815e851b63c0ee
TheGoodCoder12 <satyam.strange93@gmail.com>
2024-09-26 20:35:41 +0530

    Encountering error connecting to mongodb

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,23 @@ const app = express();
 const mongoose=require('mongoose')
 const path = require('path');
 
-mongoose.connect(process.env.DB_URL)
-
 const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'templates', 'welcome.html'));
 });
+mongoose.connect(process.env.DB_URL)
+const userSchema=new mongoose.Schema({
+    name:String,
+    age: Number
+})
+const userModel=mongoose.model("emp",userSchema)
+const emp1=new userModel({
+    name:"abc",
+    age:23
+})
+emp1.save()
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
