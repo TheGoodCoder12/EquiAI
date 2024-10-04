a318bec76911cfa83d0688ec8693cd4a0912517d
TheGoodCoder12 <satyam.strange93@gmail.com>
2024-10-04 17:35:09 +0530

    Backend of user signup/login authentication made, testing done using Postman

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,24 @@ const express = require('express');
 const app = express();
 const mongoose=require('mongoose')
 const path = require('path');
-
+const bodyParser=require('body-parser')
+const cors=require('cors')
 const port = process.env.PORT || 8080;
+const AuthRouter=require('./Routes/AuthRouter')
+require('./Models/db')
+app.use(bodyParser.json())
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/auth',AuthRouter);
 
-app.get('/', (req, res) => {
+app.get("/",(req,res)=>{
+    res.send("HOLA")
+})
+
+app.get('/welcom', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'templates', 'welcome.html'));
 });
-mongoose.connect(process.env.DB_URL)
-const userSchema=new mongoose.Schema({
-    name:String,
-    age: Number
-})
-const userModel=mongoose.model("emp",userSchema)
-const emp1=new userModel({
-    name:"abc",
-    age:23
-})
-emp1.save()
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
